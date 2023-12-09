b3542c88d2d5460c88a9289f534e2b1e8f3da8e9
Kyle Coberly <kyle.coberly@gmail.com>
2023-12-09 15:26:44 -0700

    Fix var name

--- 3/answer.test.ts
+++ 3/answer.test.ts
@@ -201,11 +201,13 @@ describe("isSymbol", () => {
       });
     },
   );
-  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].forEach((symbol) => {
-    test(`No: ${symbol}`, () => {
-      expect(isSymbol(symbol)).toBe(false);
-    });
-  });
+  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].forEach(
+    (character) => {
+      test(`No: ${character}`, () => {
+        expect(isSymbol(character)).toBe(false);
+      });
+    },
+  );
 });
 
 test("getNumberByBox gets a single digit number", () => {
